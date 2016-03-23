ef82836ccae0b04bebbca0963a4fdf8860b91bbf
Thorsten Bux <thorsten.bux_c@daqri.com>
2016-03-23 21:58:18 +1300

    Adapted build scripts for better build behaviour and to behave similar to the packaging scripts

--- share/cmake/cmakebuild/start64bitBuild.sh
+++ share/cmake/cmakebuild/start64bitBuild.sh
@@ -1,20 +1,19 @@
-rm -r CMakeFiles
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
-cmake -DCMAKE_TOOLCHAIN_FILE=../64bitLinuxConfiguration.cmake ../CMakeLibBuild/
+rm -rf ./build
+mkdir build
+cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=../../64bitLinuxConfiguration.cmake ../../CMakeLibBuild/
 make install
 
-rm -r CMakeFiles
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
-cmake -DCMAKE_TOOLCHAIN_FILE=../64bitLinuxConfiguration.cmake ../CMakeUtilBuild/
+cd ..
+rm -rf ./build
+mkdir build
+cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=../../64bitLinuxConfiguration.cmake ../../CMakeUtilBuild/
 make install
 
-rm -r CMakeFiles
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
-cmake -DCMAKE_TOOLCHAIN_FILE=../64bitLinuxConfiguration.cmake ../CMakeExamplesBuild/
+cd ..
+rm -rf ./build
+mkdir build
+cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=../../64bitLinuxConfiguration.cmake ../../CMakeExamplesBuild/
 make install
