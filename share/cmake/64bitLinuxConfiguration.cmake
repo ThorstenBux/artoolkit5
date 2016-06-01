9b77a22d255cf5096c4f454f99f3df4cb0a36dc8
Thorsten Bux <thorsten.bux_c@daqri.com>
2016-06-01 19:18:30 +1200

    Added travisci buildscript

--- share/cmake/64bitLinuxConfiguration.cmake
+++ share/cmake/64bitLinuxConfiguration.cmake
@@ -3,8 +3,11 @@ set(CMAKE_SYSTEM_NAME Linux)
 #If we do not set the C-Compiler cmake uses the system standard c compiler which is not what we want on the build system
 set(CMAKE_C_COMPILER gcc -m64)
 set(CMAKE_CXX_COMPILER g++ -m64)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=core2")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=core2")
 
-set(OUTPUT_DIR /opt/jenkins/artoolkitBuildArtifacts/64bit/)
+
+set(OUTPUT_DIR ${CMAKE_INSTALL_PREFIX})
 
 set(ARCHITECTURE x86_64)
 
